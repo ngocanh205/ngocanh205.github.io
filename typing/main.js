4143b0fca8f78189363858d08cbdd144dec3c1ca
ngocanh205 <62045431+ngocanh205@users.noreply.github.com>
2020-03-11 10:43:20 +0700

    Add files via upload

---
+++ typing/main.js
@@ -0,0 +1,37 @@
+var txt = document.getElementById('typing');
+
+var typewriter = new Typewriter(txt, {
+    //loop: true,
+    cursor: '_',
+    delay: 80
+});
+
+var pauseTime = 500;
+
+typewriter.changeDeleteSpeed(10)
+    .typeString('ngày hôm nay là ngày đặc biệt với Ánh')
+    .pauseFor(pauseTime)
+    .deleteChars(32)
+    .pauseFor(400)
+    .typeString('ngày 20 tháng 5 năm 2020')
+    .pauseFor(pauseTime)
+    .deleteAll(19)
+    .typeString('ngày mà mày lọt lòng chui ra thế giới này..')
+    .pauseFor(pauseTime)
+    .deleteAll()
+    .typeString('tận hưởng trọn vẹn ngày hôm nay nhá ! ')
+    .pauseFor(pauseTime)
+    .deleteAll()
+    .typeString('luôn luôn xinh đẹp và hạnh phúc <3')
+    .pauseFor(pauseTime)
+    .deleteAll()
+    .typeString('from pain_Q with ȴᴑⱴɘ ❤️')
+    .pauseFor(200)
+    .callFunction(function happy(){    
+        window.location.replace('../giftbox/');
+    })
+    .start()
+    
+     
+    
+    
